52b3702ad1f098fd9b885f1b030de09c26abdc2b
Oliversrensen <ossa18@student.aau.dk>
2021-11-23 10:11:02 +0100

    Boxes made

--- src/app/crypto-service.service.ts
+++ src/app/crypto-service.service.ts
@@ -20,10 +20,6 @@ export class CryptoServiceService {
     return this.http.get<Crypto>(endpoint + "/coins/all", {params: params}).pipe(catchError(this.handleError));
   }
 
-  getCryptocurrency(id: string): Observable<any> {
-    return this.http.get<Crypto>(endpoint + '/coins/' + id).pipe(catchError(this.handleError));
-  }
-
   getPrice(id: string, index: number): Observable<any> {
     return this.http.post("https://api.livecoinwatch.com/coins/single",
       JSON.stringify({currency: 'USD', code: id, meta: true}),

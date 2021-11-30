10b517045bb49c01e636097e05be103bc0ae1ebe
Oliversrensen <ossa18@student.aau.dk>
2021-11-30 16:03:01 +0100

    Commit for home

--- src/app/cryptocurrencies/cryptocurrencies.component.ts
+++ src/app/cryptocurrencies/cryptocurrencies.component.ts
@@ -1,18 +1,20 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnInit, AfterViewInit, ViewChild} from '@angular/core';
 import {Crypto} from "../crypto";
 import {ActivatedRoute} from '@angular/router';
 import {Location} from '@angular/common';
 import {CryptoServiceService} from "../crypto-service.service";
 import {Sort} from "@angular/material/sort";
-import {MatTable} from "@angular/material/table";
+import {MatCell, MatColumnDef, MatTable} from "@angular/material/table";
 import {HttpParams} from "@angular/common/http";
+import {TimeInterval} from "rxjs/internal-compatibility";
+import {delay} from "rxjs/operators";
 
 @Component({
   selector: 'app-cryptocurrencies',
   templateUrl: './cryptocurrencies.component.html',
   styleUrls: ['./cryptocurrencies.component.css']
 })
-export class CryptocurrenciesComponent implements OnInit {
+export class CryptocurrenciesComponent implements OnInit, AfterViewInit {
 
   cryptoList: Crypto[] = []
   priceCache: [string, Number][] = []
@@ -22,6 +24,8 @@ export class CryptocurrenciesComponent implements OnInit {
 
   @ViewChild(MatTable) table: MatTable<any>;
 
+  @ViewChild('finalScore') finalScore: ElementRef;
+
   params = new HttpParams()
     .set('length', 25)
     .set('sortParam', '-mentions')
@@ -29,11 +33,26 @@ export class CryptocurrenciesComponent implements OnInit {
   ngOnInit(): void {
     this.getCryptocurrencies(this.params);
 
-    window.addEventListener('load', (event) => {
-      console.log(document.getElementById('finalScore'))
-      console.log(document.readyState)
-    })
+  }
 
+  ngAfterViewInit() {
+    setTimeout(() =>
+      {
+
+        var length = this.table._contentColumnDefs.get(1)?.
+        _table._elementRef.nativeElement.getElementsByClassName('finalScore').length;
+
+        for(var i = 0; i < length; i++){
+        //  this.table._contentColumnDefs.get(1)?.
+        //  _table._elementRef.nativeElement.getElementsByClassName('finalScore')[i].nativeElement.style.color = 'red';
+        }
+
+        console.log(this.table._contentColumnDefs.get(1)?.
+        _table._elementRef.nativeElement.getElementsByClassName('finalScore')[3].style)
+        this.finalScore.nativeElement.style.color = 'red'
+
+      },
+      500);
   }
 
   //Gets currencies for the table on the frontpage
